afb67044ddbed7bd1d77376824a73b01d26b5843
dgaillard2021 <davidgaillard.pro@hotmail.fr>
2021-07-23 16:39:25 +0200

    suite commit initial

--- src/fr/eni/papeterie/bo/Voiture.java
+++ src/fr/eni/papeterie/bo/Voiture.java
@@ -1,5 +1,11 @@
 package fr.eni.papeterie.bo;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class Voiture {
+	@Getter @Setter @Data
+	private int nbPortes;
+	private String proprietaire;
 
 }
